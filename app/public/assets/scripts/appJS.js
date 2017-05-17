02540be8220d375bcd04b61ed307167b3ef46775
Thomas <tcserven@uoguelph.ca>
2017-05-16 23:06:11 -0400

    completed the reveal on scroll for the homepage, made it reusable

--- app/public/assets/scripts/appJS.js
+++ app/public/assets/scripts/appJS.js
@@ -1,9 +1,14 @@
-// import $ from 'jquery';
-
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 // this is so we can run the constructor function as soon as the site loads
 // We create a new object that uses the MobileMenu class as a blueprint, or creating a new instance of that class.
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+
+// Making our code reusable for revealonscroll
+new RevealOnScroll($(".map__country"), "85%");
+new RevealOnScroll($(".map__image-of-sa"), "90%");
+new RevealOnScroll($(".welcome__top-blog-post"), "85%");
+new RevealOnScroll($(".welcome__picture"), "85%");
+// var revealOnScroll = new RevealOnScroll();
--- app/public/assets/scripts/modules/RevealOnScroll.js
+++ app/public/assets/scripts/modules/RevealOnScroll.js
@@ -1,15 +1,72 @@
 import $ from "jquery";
+import waypoints from "../../../../../node_modules/waypoints/lib/noframework.waypoints";
 
 class RevealOnScroll {
-	constructor() {
-		this.itemsToReveal = $(".map__country");
+	constructor(els, offset) {
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
 		this.hideInitially();
+		this.createWaypoints();
 	}
 
 
 	hideInitially() {
 		this.itemsToReveal.addClass("reveal-item");
 	}
+
+	createWaypoints() {
+		// This is pointing back to the original object
+		var that = this;
+		this.itemsToReveal.each(function() {
+			// Here, this is pointing to the current DOM element
+			var currentItem = this; 
+			// creating a new instance of the waypoint class, which is from the lib. Its making a new object. This is technically a constructor function. This runs 4 times. 
+			new Waypoint({
+				// But now this is pointing to the new object
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offsetPercentage
+			});
+		});
+	}
 }
 
 export default RevealOnScroll;
+
+
+// old code
+// import $ from "jquery";
+// import waypoints from "../../../../../node_modules/waypoints/lib/noframework.waypoints";
+
+// class RevealOnScroll {
+// 	constructor() {
+// 		this.itemsToReveal = els;
+// 		this.hideInitially();
+// 		this.createWaypoints();
+// 	}
+
+
+// 	hideInitially() {
+// 		this.itemsToReveal.addClass("reveal-item");
+// 	}
+
+// 	createWaypoints() {
+// 		this.itemsToReveal.each(function() {
+// 			// Here, this is pointing to the current DOM element
+// 			var that = this; 
+// 			// creating a new instance of the waypoint class, which is from the lib. Its making a new object. This is technically a constructor function. This runs 4 times. 
+// 			new Waypoint({
+// 				// But now this is pointing to the new object
+// 				element: that,
+// 				handler: function() {
+// 					$(that).addClass("reveal-item--is-visible");
+// 				},
+// 				offset: "85%"
+// 			});
+// 		});
+// 	}
+// }
+
+// export default RevealOnScroll;
