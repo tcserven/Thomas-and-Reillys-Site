763def217dd20d348c67171bf87ae29b322d9393
Thomas <tcserven@uoguelph.ca>
2017-07-03 22:43:28 -0400

    started working on our journey

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express'),
 	  path = require('path'),
 	  bodyParser = require('body-parser');
 
-
 const port = process.env.PORT || 3000;
-
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 // serve up the public folder
@@ -24,8 +22,13 @@ app.get("/about-us", function(req, res) {
     res.render('about-us');    
 });
 
+// The four countries overview pages
 app.get("/overview", function(req, res) {    
     res.render('our-journey');    
+});
+
+app.get("/overview/bolivia", function(req, res) {    
+    res.render('countries/bolivia');    
 });
 
 
